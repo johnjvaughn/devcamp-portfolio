14f303a732702961948c2c616152b17c225488c2
John Vaughn <johnjvaughn@gmail.com>
2017-04-03 13:59:58 +0000

    implement show and destroy portfolio actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -36,6 +36,13 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'The record was removed.' }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
