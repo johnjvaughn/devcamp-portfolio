d87ae99480c55dfb11362eaa8dd425338ac9673e
John Vaughn <johnjvaughn@gmail.com>
2017-04-08 18:35:11 +0000

    initial file upload system via carrierwave

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,15 +6,14 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+  
   def self.angular
     where(subtitle: 'Angular')
   end
   
-  # def self.by_position
-  #   order('position ASC')
-  # end
   scope :by_position, -> { order('position ASC') }
-  
   scope :ruby_on_rails_items, -> { where(subtitle: 'Ruby on Rails') }
   
   after_initialize :set_defaults
