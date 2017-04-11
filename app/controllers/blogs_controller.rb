185129f9c081d8ba18719727773c9ed4a1c66fae
John Vaughn <johnjvaughn@gmail.com>
2017-04-11 15:14:05 +0000

    index blogs in newest to oldest order

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,7 +8,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    @blogs = Blog.order('created_at DESC').page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
