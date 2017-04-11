973701488092c768865ae1dc524977bf9e20d3ad
John Vaughn <johnjvaughn@gmail.com>
2017-04-11 14:02:06 +0000

    ActionCable complete for comments

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -1,11 +1,12 @@
 class GuestUser < User
-  attr_accessor :name, :first_name, :last_name, :email, :roles
+  attr_accessor :name, :first_name, :last_name, :email, :id, :roles
   
-  def initialize(name, email)
+  def initialize(name="Guest User", email="guest@user.com", id=0)
     self.name = name
     self.email = email
     self.first_name = name.split.first
     self.last_name = name.split.last
+    self.id = id > 0 ? id : self.object_id
     self.roles = ['']
   end
   
